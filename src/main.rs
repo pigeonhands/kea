5f92babc8787f4971d8612f63c43d0d8b3e1b449
Sam <sam.mackenzie@live.com>
2019-02-21 03:10:08 +1300

    started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Arg, App, SubCommand};
+
+use std::error::Error;
+
+fn try_main() -> Result<(), Box<Error>> {
+    let matches = App::new("kia")
+                    .about("Rust aur client for arch linux")
+                    .author("Sam M.")
+                    .version(env!("CARGO_PKG_VERSION"))
+        .arg(Arg::with_name("package")
+            .index(1)
+            .help("package to search aur for")
+            .required(true))
+            .get_matches();
+
+    
+    match matches.subcommand(){
+        ("remove", Some(remove))=>{
+
+        },
+       _ => { //search aur
+            let package = matches.value_of("package").unwrap();
+            println!("{}", package)
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match try_main(){
+        Ok(()) => {},
+        Err(e) => {
+            eprintln!("Error! {}", e);
+        }
+    }
 }
